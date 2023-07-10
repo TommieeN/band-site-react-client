51c9669d49e0c67485e16cb441afe8f187326b85
TommyNguyen <timsnguyen@hotmail.com>
2023-07-10 11:03:06 -0700

    finish tablet breakpoint for shows

--- src/components/ShowsHero.jsx
+++ src/components/ShowsHero.jsx
@@ -1,11 +1,11 @@
 const ShowsHero = () => {
   return (
-    <div className="h-[45vh] bg-hero-shows hero">
+    <div className="h-[45vh] bg-hero-shows hero md:h-[75vh]">
       <div className="p-[1rem]">
-        <p className="py-[1rem] text-[0.85rem] text-primary-light">Beautiful Beasts Album</p>
-        <h1 className="text-[1.6rem] font-semibold text-primary-light">Queen of Yellow X For The Stings</h1>
+        <p className="py-[1rem] text-[0.85rem] text-primary-light md:text-[1.4rem]">Beautiful Beasts Album</p>
+        <h1 className="text-[1.6rem] font-semibold text-primary-light md:text-[4.4rem] md:font-normal md:w-[75%]">Queen of Yellow X For The Stings</h1>
       </div>
-      <div className="px-[1rem]">
+      <div className="px-[1rem] md:w-[60%]">
       <iframe
         width="100%"
         height="100"
--- src/components/ShowsList.jsx
+++ src/components/ShowsList.jsx
@@ -13,27 +13,32 @@ const ShowsList = () => {
   }, []);
 
   return (
-    <div>
-      <h2 className="font-medium text-[1.5rem] p-[1rem] pb-0">Shows</h2>
+    <div className="md:px-[1.5rem]">
+      <h2 className="font-medium text-[1.5rem] p-[1rem] pb-0 md:text-[2rem] md:font-semibold md:py-[2rem]">Shows</h2>
+      <div className="hidden md:flex md:justify-start md:px-[1rem] md:text-silver md:text-[0.7rem] md:font-semibold">
+      <p className="md:w-[25%]">DATE</p>
+      <p className="md:w-[25%]">VENUE</p>
+      <p className="md:w-[25%]">LOCATION</p>
+      </div>
       <ul>
         {shows.map((show) => {
           return (
-            <li className="border-b p-[1rem] text-[0.8rem]" key={show.id}>
-              <div className="py-[1rem]">
+            <li className="border-b p-[1rem] text-[0.8rem] md:flex md:justify-around md:items-center md:flex-1" key={show.id}>
+              <div className="py-[1rem] md:w-[25%]">
                 <h3 className="shows-text">DATE</h3>
                 <p className="text-[0.8rem] font-semibold">
                   {showsTimeStampConverter(show.date)}
                 </p>
               </div>
-              <div className="pb-[1rem]">
+              <div className="pb-[1rem] md:p-0 md:w-[25%]">
                 <h3 className="shows-text">VENUE</h3>
                 <p>{show.place}</p>
               </div>
-              <div className="pb-[1rem]">
+              <div className="pb-[1rem] md:p-0 md:w-[25%]">
                 <h3 className="shows-text">LOCATION</h3>
                 <p>{show.location}</p>
               </div>
-              <div className="flex">
+              <div className="flex md:w-[25%]">
                 <button className="font-medium text-[1rem] w-[100%] p-[0.7rem] rounded bg-primary-dark text-primary-light">
                   BUY TICKETS
                 </button>
